6d1eb1df38f4f81c52b352f7d1fef89a05150872
Piotr Balon <balonpiotrek@gmail.com>
2019-11-09 17:40:25 +0100

    Implement DataTestMethod

--- UnitTestBank/UnitTest1.cs
+++ UnitTestBank/UnitTest1.cs
@@ -7,21 +7,36 @@ namespace UnitTestBank
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [DataTestMethod]
+        [DataRow("mol", 1000)]
+        [DataRow("abc", 100.23)]
+        [DataRow("aaa", 0)]
+        public void Konstruktor_PoprawneDane_PoprawneWyniki(string klient, double kwota)
         {
-            //Arrange
-            string klient = "kowalski";
-            decimal kwota = 1000;
+            // Arrange
+            //string klient = "kowalski";
+            //decimal kwota = 1000;
 
-            //Act
-            Konto k = new Konto(klient, kwota);
+            // Act
+            Konto k = new Konto(klient, (decimal)kwota);
 
-            //Assert
+            // Assert
             Assert.AreSame(klient, k.Nazwa);
-            Assert.AreEqual(kwota, k.Bilans);
+            Assert.AreEqual((decimal)kwota, k.Bilans);
             Assert.IsTrue(k.Bilans >= 0);
 
+        }
+
+
+        [DataTestMethod]
+        [DataRow("mol", -100)]
+        [DataRow("mol", -0.00000000001)]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Konstruktor_UjemnaKwota_Wyjatek(string klient, double kwota)
+        {
+            // Act
+            Konto k = new Konto(klient, (decimal)kwota);
+            // powinien być zgłoszony wyjątek
         }
     }
 }
