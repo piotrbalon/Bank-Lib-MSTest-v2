2945adf315348d9c3ff9517a474ded4996866e09
Piotr Balon <balonpiotrek@gmail.com>
2019-11-09 17:07:17 +0100

    Add project files.

---
+++ BankLib/Konto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BankLib
+{
+    public class Konto
+    {
+        private string klient;  //nazwa klienta
+        private decimal bilans;  //aktualny stan środków na koncie
+        private bool zablokowane = false; //stan konta
+
+        public string Nazwa => klient; // get, read-only
+        public decimal Bilans => bilans; // get
+        public bool Blokada => zablokowane; //get
+        public Konto (string klient, decimal bilansNaStart = 0)
+        {
+            this.klient = klient;
+            if (bilansNaStart < 0)
+                throw new ArgumentOutOfRangeException("Kwota nie może być ujemna");
+
+            this.bilans = bilansNaStart;
+        }
+
+        public void Wplata(decimal kwota) => throw new NotImplementedException();
+
+        public void Wyplata(decimal kwota)
+        {
+            if (kwota < 0)
+                throw new ArgumentOutOfRangeException("Kwota nie może być ujemna");
+            bilans -= kwota;
+        }
+
+    }
+}
---
+++ UnitTestBank/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestBank")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestBank")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("76a839c4-dfb0-4e3d-98d1-3abfc016d28d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
